6df76b4a9a28f6ae212cedf602cd7066b7b5ec24
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-11-09 13:28:54 +0100

    cleanup

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
@@ -5,7 +6,9 @@ import { BookRatingService } from '../shared/book-rating.service';
 @Component({
   selector: 'br-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  // Vorsicht: später hier ein Bug (bei Ajax)!
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit {
 
@@ -34,11 +37,19 @@ export class DashboardComponent implements OnInit {
 
   doRateDown(book: Book): void {
     const ratedBook = this.rs.rateDown(book);
-    console.table(ratedBook);
+    this.update(ratedBook);
   }
 
   doRateUp(book: Book): void {
     const ratedBook = this.rs.rateUp(book);
-    console.table(ratedBook);
+    this.update(ratedBook);
+    // fieser CD bug
+    // this.books[2].title = 'fooo';
+  }
+
+  update(ratedBook: Book): void {
+    this.books = this.books
+      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+      .sort((a, b) => b.rating - a.rating);
   }
 }
