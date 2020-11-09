35ec50ad21440e26780de0ea6c979a1543cfbf4a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-11-09 14:12:22 +0100

    fixing: book-component

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -16,6 +16,12 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = {
+      isbn: 'TEST',
+      title: 'test',
+      description: 'blubb',
+      rating: 1
+    };
     fixture.detectChanges();
   });
 
