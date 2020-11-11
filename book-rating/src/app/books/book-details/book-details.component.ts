9d8d692e320ce50a1e005c602174b09f54d01fe4
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-11-11 13:04:41 +0100

    rxjs: unSubscribe via OnDestroy

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import { of, Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent implements OnInit, OnDestroy {
 
   isbn: string;
+  sub: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -20,11 +21,24 @@ export class BookDetailsComponent implements OnInit {
 
     //// ---- RxJS playground
 
-    of('😀', '😎', '😈').subscribe(
-      e => console.log(e),
-      err => console.log('ERROR', err),
-      () => console.log('COMPLETE')
-      );
+    // building block: OBSERVER
+    const observer = {
+      next:  e => console.log(e),
+      error: err => console.log('ERROR', err),
+      complete: () => console.log('COMPLETE')
+    };
+
+    // const observable = of('😀', '😎', '😈');
+    const observable = timer(0, 100);
+
+    this.sub = observable.subscribe(observer);
+
+    // setTimeout(() => subscription.unsubscribe(), 5000);
   }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
 
 }
